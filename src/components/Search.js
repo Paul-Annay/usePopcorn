a254cf850bc8f218e30acee79899f1687c600cc3
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-12-02 20:06:14 +0600

    Enter keypress shifts focus on searchbar : Feature implemented using useRef

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,34 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(function () {
+        function callback(e) {
+            if (document.activeElement === inputEl.current) {
+                return;
+            }
+
+            if (e.code === "Enter") {
+                inputEl.current.focus();
+                setQuery("");
+            }
+        }
+        document.addEventListener("keydown", callback);
+
+        return function () {
+            document.removeEventListener("keydown", callback);
+        };
+    });
+
     return (
         <input
             type='text'
             placeholder='Search movies...'
             className='search'
             value={query}
+            ref={inputEl}
             onChange={(e) => setQuery(e.target.value)}
         />
     );
